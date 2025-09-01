500bd3d5c120b77c614da5162ee7849c2b3de537
Umbhorn Ungkulpasvich <umbhorn.ung@gmail.com>
2025-09-01 12:45:57 +0900

    update 2025

--- supermain.py
+++ supermain.py
@@ -1,18 +1,34 @@
 import streamlit as st
 import webapp_v3
+import webapp_v4
 import extract_file_name
 import roc_v1
 import Logisitic_regression
+import subtype
+import line
+import group_v1
+import line
+
 
 # Create a menu to select the app
-app_choice = st.sidebar.selectbox("Select App", ["Chemotaxis_assay_index", "Total_index", "ROC","Logisitic_regression"])
+app_choice = st.sidebar.selectbox("Select App", ["Chemotaxis_assay_index","Chemotaxis_index_line_graph","PCA","Chemotaxis_index_strain","Subtype", "Total_index", "ROC","Logisitic_regression","Line"])
 
 # Run the selected app
 if app_choice == "Chemotaxis_assay_index":
     webapp_v3.run(key="Chemotaxis_assay_index")  # Provide a unique key here
+elif app_choice == "Chemotaxis_index_line_graph":
+    line.run(key="Chemotaxis_index_line_graph")  # Provide a unique key here
+elif app_choice == "PCA":
+    group_v1.run(key="PCA")  # Provide a unique key here
+elif app_choice == "Chemotaxis_index_strain":
+    webapp_v4.run(key="Chemotaxis_index_strain")  # Provide a unique key here
+elif app_choice == "Subtype":
+    subtype.run(key="subtype")  # Provide a unique key here
 elif app_choice == "Total_index":
     extract_file_name.run(key="EXTRACT_FILE_APP")  # Provide a unique key here
 elif app_choice == "ROC":
     roc_v1.run(key="ROC_APP")  # Provide a unique key here
 elif app_choice == "Logisitic_regression":
-    Logisitic_regression.run(key="Logisitic_regression")  # Provide a unique key here
+    Logisitic_regression.run(key="logreg_app")  # Provide a unique key here
+elif app_choice == "Line":
+    line.run(key="Line")  # Provide a unique key here
--- webapp_v3.py
+++ webapp_v3.py
@@ -1,54 +1,82 @@
-import streamlit as st
-import pandas as pd
-import openpyxl
-import plotly.express as px
-import numpy as np
-from PIL import Image
+import streamlit as st # makes the website app
+import pandas as pd # helpd read and work with excel tables
+import openpyxl # reads excel files
+import plotly.express as px # make colorful graphs
+import numpy as np # helps with numbers and maths
 import plotly.graph_objects as go
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA # helps with smart analysis (such as PCA)
+from pandas.api.types import is_object_dtype, is_datetime64_any_dtype, is_categorical_dtype, is_numeric_dtype
 
+# function to add filters to data
+## input: table (called df), output: a filtered table
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a UI on top of a dataframe to let viewers filter columns
-    Args:
+    Args: # the *arg (tuple) means passing an arbitrary number of arguments and
+    **kwargs (dict) means passing an arbitrary number of arguments with keywords.
         df (pd.DataFrame): Original dataframe
     Returns:
         pd.DataFrame: Filtered dataframe
     """
+    # this adds a checkbox.
     modify = st.checkbox("Add filters")
 
-    if not modify:
-        return df
+    if not modify: #if not check it, it just shows the full table.
+        return df  #if do check it, the filter options will appear.
 
-    df = df.copy()
+    df = df.copy() # make a copy of the table to avoid accidentally changing the original table.
 
-    for col in df.columns:
-        if is_object_dtype(df[col]):
+    # go through each column (like 'specimen', 'conc', 's_index')
+    for col in df.columns: # loop-through each column
+        #if the column is a text, it tries to change it into dates
+        if is_object_dtype(df[col]): # if a column looks like text, we try to see if it's date(like"2024-05-01")
             try:
                 df[col] = pd.to_datetime(df[col])
-            except Exception:
+            except Exception: #if it fails, we skip it (don't break a app)
                 pass
-
+        # remove timezone, just a cleanup step, removes timezone info from date columns
         if is_datetime64_any_dtype(df[col]):
             df[col] = df[col].dt.tz_localize(None)
 
+    #Build filter UI by creating container for filter UI, this creates a place to put the filters.
     modification_container = st.container()
 
     with modification_container:
+        # pick column you want to filter
         to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
 
+        # filter logic for each column
+        ## split the screen
         for column in to_filter_columns:
-            left, right = st.columns((1, 20))
+            left, right = st.columns((1, 20)) # left: a little arrow, right: the filter UI
             left.write("↳")
 
+            #for category-type or small lists
             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                # is_categorical_dtype(array or dtype) return boolean
+                # .nunique is count number of distinct elements in specified axis return series with number of distinct elements and ignore NaN
+                # column: specimen, values: lung, colon, stomach
                 user_cat_input = right.multiselect(
                     f"Values for {column}",
                     df[column].unique(),
                     default=list(df[column].unique()),
                 )
+                '''
+                right.multiselect: creates a multiselect dropdown inside the right column layout from st.column(...)
+                f"Values for {colunm}: the lable shown in the dropdown (e.g. value for cancer type)
+                df[column].unique(): the selectable options- all unique values from the specified column.
+                default = list(df[column].unique()): all options are pre=selected by default
+                (i.e., no filtering happens until the user changes the selection)
+                user_cat_input: the output list of selected values, which you can then use to filter the dataframe
+                If column = "cancer_type" and values are ["lung", "breast", "colorectal"], the widget will:
+                        - Display a multiselect with these 3 values.
+                        - Have all of them selected by default.
+                        - Let the user filter to, say, just "lung" and "breast".
+                '''
+                # keeps only the selected values
                 df = df[df[column].isin(user_cat_input)]
 
+            # for numeric data: (column:s_index)
             elif is_numeric_dtype(df[column]):
                 _min = float(df[column].min())
                 _max = float(df[column].max())
@@ -60,8 +88,9 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                     (_min, _max),
                     step=step,
                 )
+                #slider for min to max value, keeps only rows between those numbers
                 df = df[df[column].between(*user_num_input)]
-
+            #for date columns: column: collected_date
             elif is_datetime64_any_dtype(df[column]):
                 user_date_input = right.date_input(
                     f"Values for {column}",
@@ -73,18 +102,48 @@ def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
                 if len(user_date_input) == 2:
                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
                     start_date, end_date = user_date_input
+                    # see a calendar to pick a start and end date/ keeps only rows within the date range
                     df = df.loc[df[column].between(start_date, end_date)]
 
+            #for text columns(fallback)
             else:
                 user_text_input = right.text_input(
                     f"Substring or regex in {column}",
                 )
-                if user_text_input:
+                if user_text_input: # you can type a word or part of a word
                     df = df[df[column].str.contains(user_text_input)]
+                #keep only rows that contain the text you typed.
+    return df # return filtered table
 
-    return df
 
+# calculate PCA loadings
+def calculate_pca_loadings(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculate PCA and return loadings
+    Args:
+        df (pd.DataFrame): Dataframe containing the data for PCA
+    Returns:
+        pd.DataFrame: DataFrame containing PCA loadings
+    """
+    features = ['specimen', 'conc', 's_index']
+    X = df[features]
+    y = df['s_index']
+
+    X_enc = pd.get_dummies(X, columns=['specimen', 'conc'])
+    X_norm = (X_enc - X_enc.mean()) / X_enc.std()
+
+    pca = PCA(n_components=3)
+    pca.fit(X_norm)
+
+    loadings_df = pd.DataFrame(pca.components_.T, columns=[f'PC{i+1}' for i in range(pca.n_components_)])
+    loadings_df.index = X_enc.columns
+
+    return loadings_df
+
+# the main program
 def run(key):
+
+    # upload a file (shows file name, file size, sheet names inside it)
     st.sidebar.title("Upload File")
     data_file = st.sidebar.file_uploader("Upload Excel file", type=['xlsx'], key=key)
 
@@ -101,110 +160,43 @@ def run(key):
         st.sidebar.markdown("----")
 
         st.header("Sheet view")
+        #show the excel sheet
         sheet_selector = st.sidebar.selectbox("Select sheet:", wb.sheetnames)
         df = pd.read_excel(data_file, sheet_selector)
         st.markdown(f"### Currently Selected worksheet: `{sheet_selector}`")
 
+        #the user filter the data (pick certain dates, values, look at ranges)
         st.dataframe(filter_dataframe(df))
 
         mean_result = st.button("Analyze data...")
+        # analyze the data (how many samples for each cancer type, avg, min, max values)
         if mean_result:
             st.header('Evaluation of data')
             df_mean = df.groupby(['specimen']).agg({"s_index":["mean"]})
             df_cal = df.groupby(['specimen', 'conc']).agg({"s_index":["count","mean","median","min","max","std","var"]})
-            st.write(df_cal) 
+            st.write(df_cal)
 
             df['group'] = df['specimen'].str.cat(df['conc'], sep='')
 
-            st.subheader('p-value')
-            st.caption('***Tukey’s honestly significant difference (HSD) test performs pairwise comparison of means for a set of samples. Whereas ANOVA (e.g. f_oneway) assesses whether the true means underlying each sample are identical, Tukey’s HSD is a post hoc test used to compare the mean of each sample to the mean of each other sample.***')
-            
-            from statsmodels.stats.multicomp import pairwise_tukeyhsd
-            from statsmodels.stats.multicomp import MultiComparison
-
-            mc = MultiComparison(df['s_index'], df['group'])
-            result = mc.tukeyhsd()
-            st.code(result)
-            st.caption('*In this table, the "p-adj" column means p-value.')
-            st.caption('*The "reject" column shows that true is "Significant difference".')
-
             st.header('Bar plot')
-            kd1 = df.iloc[1, 1]
-            kd2 = df.iloc[1, 5]
-            st.subheader('nematode: {}, type: {}'.format(kd1, kd2))
-            
+            selected_nematode = df.iloc[1, 1]
+            selected_type = df.iloc[1, 5]
+            st.subheader('Nematode: {}, Type: {}'.format(selected_nematode, selected_type))
+
+
+
             agg_df = df.groupby(['specimen', 'conc'], as_index=False).agg({'s_index':['mean', 'std']})
             agg_df.columns = ['specimen', 'conc', 'mean', 'sd']
             agg_df['SE'] = agg_df['sd'] / np.sqrt(df.groupby(['specimen', 'conc']).size().values)
 
-            fig = px.bar(agg_df, x=('specimen'), y='mean', error_y='SE', color='conc', barmode='group', range_y=(-0.20, 0.20), width=600, height=400, color_discrete_sequence=px.colors.qualitative.Pastel2).update_layout(
+            #show graphs
+            fig = px.bar(agg_df, x='specimen', y='mean', error_y='SE', color='conc', barmode='group', range_y=(-0.20, 0.20), width=600, height=400, color_discrete_sequence=px.colors.sequential.Blues[2:]).update_layout(
                 xaxis_title='Cancer type', yaxis_title='Index(-)', paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 
             st.plotly_chart(fig, use_container_width=True)
 
             st.header('Violin and Scatter plot')
 
-            fig2 = px.violin(df, x='specimen', y='s_index', box=True, points='all', color='conc', violinmode='group', range_y=(-0.75, 0.75), width=600, height=400, color_discrete_sequence=px.colors.qualitative.Pastel2).update_layout(
+            fig2 = px.line(df, x='conc', y='s_index', color='conc', markers=True, range_y=(-0.75, 0.75), width=600, height=400, color_discrete_sequence=px.colors.sequential.Blues[2:]).update_layout(
                 xaxis_title='Cancer type', yaxis_title='Index(-)', paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
-            
             st.plotly_chart(fig2, use_container_width=True)
-
-            st.header('PCA plot and 3D plot')
-
-            df_pivot = pd.pivot_table(df, values='s_index', index='conc', columns='specimen')
-            
-            pca = PCA(n_components=2)
-            principal_components = pca.fit_transform(df_pivot.values)
-            df_pc = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
-            df_pc['specimen'] = df_pivot.index
-
-            st.subheader('PCA plot average')
-            fig_pca = px.scatter(df_pc, x='PC1', y='PC2', color='specimen', title='PCA Plot')
-            
-            st.plotly_chart(fig_pca)
-
-            st.subheader('3D plot average')
-
-            x_vals = df['specimen'].unique()
-            y_vals = df['conc'].unique()
-            z_vals = df_pivot.values
-
-            fig_surface = go.Figure(data=[go.Surface(z=z_vals, x=x_vals, y=y_vals)])
-            fig_surface.update_layout(title='3D Surface Plot')
-            st.plotly_chart(fig_surface)
-
-            st.subheader('PCA plot')
-
-            features = ['specimen', 'conc', 's_index']   
-
-            X = df[features]
-            y = df['s_index']
-
-            X_enc = pd.get_dummies(X, columns=['specimen', 'conc'])
-
-            X_norm = (X_enc - X_enc.mean()) / X_enc.std()
-
-            pca = PCA(n_components=3)
-            pca.fit(X_norm)
-
-            transformed_data = pca.transform(X_norm)
-
-            pc_cols = ['PC1', 'PC2', 'PC3']
-            pca_df = pd.DataFrame(transformed_data, columns=pc_cols)
-            pca_df['specimen'] = X['specimen']
-            pca_df['conc'] = X['conc']
-
-            fig = px.scatter_3d(pca_df, x='PC1', y='PC2', z='PC3', color='specimen',
-                                symbol='conc', hover_name='specimen',
-                                hover_data={'conc': True, 'PC1': False, 'PC2': False, 'PC3': False})
-            st.plotly_chart(fig)
-
-            subset_df = df.loc[df['conc'].isin(['1_10(1)', '1_10(2)', '1_10(3)','1_10(4)'])]
-
-            fig = px.scatter_3d(subset_df, x='conc', y='specimen', z='s_index', color='conc', symbol='conc')
-            fig.update_layout(scene=dict(xaxis_title='conc', yaxis_title='specimen', zaxis_title='s_index'))
-
-            st.plotly_chart(fig)
-
-if __name__ == "__main__":
-    run(key="Chemotaxis_assay_index")
